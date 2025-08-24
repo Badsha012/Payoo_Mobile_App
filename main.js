90d0729c2ba47810f8487fb00b6ae4d1200e4c38
Badsha012 <badshasheikh361@gmail.com>
2025-08-23 17:42:23 -0700

    five commit

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const validin=1234
 
+// function 
+function getInputValueNumber(id){
+     const inputField=document.getElementById(id)
+     const inputFieldValue=inputField.value
+     const inputFieldValueNumber=parseInt(inputFieldValue)
+     return inputFieldValueNumber
+}
+
+
+
 // add money btn
 
 document.getElementById("addMoney").addEventListener('click',function(e){
 e.preventDefault()
-console.log("add money btm click")
+// console.log("add money btm click")
 
 const bank=document.getElementById("bank").value
 
 const account=document.getElementById("account").value
-const addMoney=parseInt(document.getElementById("add-money").value)
-const pin=parseInt(document.getElementById("pin-number").value)
+// const addMoney=parseInt(document.getElementById("add-money").value)
+ const addMoney=getInputValueNumber("add-money")
+
+// const pin=parseInt(document.getElementById("pin-number").value)
+const pin=getInputValueNumber("pin-number")
 
 // console.log(bank,account,addMoney,pin)
 const aviable=parseInt(document.getElementById("aviable-balance").innerText)
@@ -32,8 +45,13 @@ document.getElementById("aviable-balance").innerText =totalNewAmount
 // cash out 
 document.getElementById("withdraw-btn").addEventListener('click',function(e){
     e.preventDefault()
-    console.log("withdraw is money")
 
+    const amount=getInputValueNumber("withraw-amount")
+    // const amount=parseInt(document.getElementById("withraw-amount").value)
+    const availableBlance=parseInt(document.getElementById("aviable-balance").innerText)
+    // console.log(amount,availableBlance)
+    const totalNewAmount=availableBlance - amount
+    document.getElementById("aviable-balance").innerText=totalNewAmount
 })
 
 
